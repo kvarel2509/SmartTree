7850b2b2e650e6dbbc47c88617c0f430adec7222
kvarel <kvarel@mail.ru>
2022-11-04 23:11:38 +0300

    Added the ability to insert html code.
    js structure redesigned according to the MVC pattern

--- tree/static/tree/dialog.js
+++ tree/static/tree/dialog.js
@@ -1,67 +1,233 @@
-class DialogStage {
-	startStage
-	activeStage
-	prevStage
+class Subject {
 	observers = []
-	validator = new ChangeStageValidator(this)
 
-	constructor() {
-		this.activeStage = this.getActiveStage()
-		this.startStage = this.activeStage
-		this.prevStage = null
-		this.addObservers()
+	addObserver(observer) {
+        this.observers.push(observer)
+    }
+
+    notifyObservers() {
+        this.observers.forEach(e => e.update())
+    }
+}
+
+
+class Observer {
+	node
+	subject
+
+	constructor(node, subject) {
+		this.subject = subject
+		this.subject.addObserver(this)
+		this.node = node
 	}
 
-	addObservers() {
-		this.observers.push(new StepAgainButtonStyleController(document.querySelector('#step-back'), this))
+	update() {}
+}
+
+
+class DialogStageModel extends Subject {
+    startStage
+    activeStage
+    prevStage
+
+    constructor(stage) {
+		super()
+        this.startStage = stage
+        this.activeStage = stage
+        this.prevStage = null
+    }
+
+    setActiveStage(stage) {
+        this.prevStage = this.activeStage
+        this.activeStage = stage
+        this.notifyObservers()
+    }
+
+    startAgain() {
+        this.activeStage = this.startStage
+        this.prevStage = null
+        this.notifyObservers()
+    }
+
+    stepBack() {
+        this.activeStage = this.prevStage
+        this.prevStage = null
+        this.notifyObservers()
+    }
+}
+
+
+class DynamicFieldModel extends Subject {
+	label
+	value
+	permanent
+
+	constructor(label, value, permanent) {
+		super()
+		this.label = label
+		this.value = value
+		this.permanent = permanent
 	}
 
-	getActiveStage() {
-		return (this.activeStage) ? this.activeStage : document.querySelector('.dialog__stage.active')
-	}
-
-	setActiveStage(stage) {
-		if (!this.validator.validate()) {
-			alert('Необходимо заполнить все поля для ввода текста')
-			return
-		}
-
-		let newActiveStage = stage
-		if (newActiveStage === this.activeStage) return
-		if (this.activeStage) this.activeStage.classList.remove('active')
-		this.prevStage = this.activeStage
-		this.activeStage = newActiveStage
-		this.activeStage.classList.add('active')
+	setValue(value) {
+		this.value = value
 		this.notifyObservers()
 	}
 
-	startAgain() {
-		if (!this.startStage || !this.activeStage) return
-		this.activeStage.classList.remove('active')
-		this.activeStage = this.startStage
-		this.prevStage = null
-		this.activeStage.classList.add('active')
+	resetValue() {
+		if (this.permanent) return
+		this.value = ''
 		this.notifyObservers()
-	}
-
-	stepBack() {
-		if (!this.prevStage || !this.activeStage) return
-		this.activeStage.classList.remove('active')
-		this.activeStage = this.prevStage
-		this.prevStage = null
-		this.activeStage.classList.add('active')
-		this.notifyObservers()
-	}
-
-	notifyObservers() {
-		this.observers.forEach(e => e.update())
 	}
 }
 
 
-class ChangeStageValidator {
+class InputDynamicFieldObserver extends Observer {
+	update() {
+		this.node.value = this.subject.value
+	}
+}
+
+
+class SpanDynamicFieldObserver extends Observer {
+	update() {
+		this.node.innerHTML = this.subject.value
+	}
+}
+
+
+class StepBackButtonObserver extends Observer {
+	activeState = new StepBackButtonActiveState(this)
+	disabledState = new StepBackButtonDisabledState(this)
+	state = this.disabledState
+
+	constructor(node, subject) {
+		super(node, subject)
+		this.state.activateState()
+	}
+
+	setState() {
+		if (this.state === this.activeState) this.state = this.disabledState
+		else this.state = this.activeState
+		this.state.activateState()
+	}
+
+	update() {
+		this.state.update()
+	}
+}
+
+
+class StepBackButtonActiveState {
+	controller
+	classes = ['btn-navigation']
+
 	constructor(controller) {
 		this.controller = controller
+	}
+
+	update() {
+		if (!this.controller.subject.prevStage && this.controller.state === this) {
+			this.classes.forEach(e => this.controller.node.classList.remove(e))
+			this.controller.setState()
+		}
+	}
+
+	activateState() {
+		this.classes.forEach(e => this.controller.node.classList.add(e))
+	}
+}
+
+class StepBackButtonDisabledState {
+	controller
+	classes = ['btn-navigation__disabled']
+
+	constructor(controller) {
+		this.controller = controller
+	}
+
+	update() {
+		if (this.controller.subject.prevStage && this.controller.state === this) {
+			this.classes.forEach(e => this.controller.node.classList.remove(e))
+			this.controller.setState()
+		}
+	}
+
+	activateState() {
+		this.classes.forEach(e => this.controller.node.classList.add(e))
+	}
+}
+
+
+class DynamicFieldsController {
+	groups = new Map()
+
+	addGroup(label, value, permanent) {
+		let model = new DynamicFieldModel(label, value, permanent)
+		this.groups.set(label, model)
+		return model
+	}
+	
+	setValue(label, value) {
+		if (!this.groups.has(label)) return
+		this.groups.get(label).setValue(value)
+	}
+
+	reset() {
+		this.groups.forEach(e => e.resetValue())
+	}
+
+	copyDynamicFieldValue(label) {
+        navigator.clipboard.writeText(this.groups.get(label).value).then(() => {})
+    }
+}
+
+
+class DialogStageController {
+    validator
+	dialogStageModel
+
+	constructor(dialogStageModel) {
+		this.dialogStageModel = dialogStageModel
+		this.validator = new ChangeStageValidator(this.dialogStageModel)
+	}
+
+    setActiveStage(stage) {
+        if (this.dialogStageModel.activeStage === stage) return 
+		
+        if (!this.validator.validate(this.dialogStageModel.activeStage)) {
+			alert('Необходимо заполнить все поля для ввода текста')
+			return
+		}
+		
+        this.dialogStageModel.activeStage.classList.remove('active')
+        this.dialogStageModel.setActiveStage(stage)
+        this.dialogStageModel.activeStage.classList.add('active')
+    }
+
+    startAgain() {
+        if (!confirm('Будет начат новый диалог и сброшены значения динамичных полей')) return
+        
+        this.dialogStageModel.activeStage.classList.remove('active')
+        this.dialogStageModel.startAgain()
+        this.dialogStageModel.activeStage.classList.add('active')
+    }
+
+    stepBack() {
+        if (!this.dialogStageModel.prevStage) return
+        this.dialogStageModel.activeStage.classList.remove('active')
+        this.dialogStageModel.stepBack()
+        this.dialogStageModel.activeStage.classList.add('active')
+    }
+	
+}
+
+
+class ChangeStageValidator {
+	model
+
+	constructor(model) {
+		this.model = model
 	}
 
 	validate() {
@@ -71,7 +237,7 @@ class ChangeStageValidator {
 	}
 
 	check_input_fields_filled() {
-		let fields = this.controller.activeStage.querySelectorAll('input.billet-item')
+		let fields = this.model.activeStage.querySelectorAll('input.billet-item')
 		for (let field of fields) {
 			if (field.value.length === 0) {
 				return false
@@ -81,110 +247,30 @@ class ChangeStageValidator {
 	}
 }
 
-// ------------------------------------------------------------------------------------------
-// Базовый класс для управлением состояниями
-class BaseStyleController {
-	state
-	lead
-	slave
 
-	update() {this.state.update()}
-	setState() {}
-}
+class Dialog {
+	static dialogStageController
+	static dynamicFieldsController
 
-// Базовый класс состояния
-class BaseState {
-	update() {}
-}
-// ------------------------------------------------------------------------------------------
-
-
-class StepAgainButtonStyleController extends BaseStyleController {
-	activeState = new StepAgainButtonActiveState(this)
-	disabledState = new StepAgainButtonDisabledState(this)
-
-	constructor(slave, lead=null) {
-		super();
-		this.slave = slave
-		this.lead = lead
-		this.setState()
-	}
-
-	setState() {
-		super.setState()
-		if (!this.state) this.state = this.disabledState
-		else if (this.state === this.activeState) this.state = this.disabledState
-		else this.state = this.activeState
-		this.state.activateState()
-	}
-}
-
-
-class StepAgainButtonActiveState extends BaseState {
-	controller
-	classes = ['btn-navigation']
-
-	constructor(controller) {
-		super();
-		this.controller = controller
-	}
-
-	update() {
-		if (!this.controller.lead.prevStage && this.controller.state === this) {
-			this.classes.forEach(e => this.controller.slave.classList.remove(e))
-			this.controller.setState()
-		}
-	}
-
-	activateState() {
-		this.classes.forEach(e => this.controller.slave.classList.add(e))
-	}
-}
-
-class StepAgainButtonDisabledState extends BaseState {
-	controller
-	classes = ['btn-navigation__disabled']
-
-	constructor(controller) {
-		super();
-		this.controller = controller
-	}
-
-	update() {
-		if (this.controller.lead.prevStage && this.controller.state === this) {
-			this.classes.forEach(e => this.controller.slave.classList.remove(e))
-			this.controller.setState()
-		}
-	}
-
-	activateState() {
-		this.classes.forEach(e => this.controller.slave.classList.add(e))
-	}
-}
-
-
-function setDynamicValue(event) {
-	document.querySelectorAll(`span[data-target=${event.target.dataset.target}]`).forEach(e => e.innerHTML = event.target.value)
-	document.querySelectorAll(`input[data-target=${event.target.dataset.target}]`).forEach(e => e.value = event.target.value)
-}
-
-function copyDynamicFieldValue(id) {
-	navigator.clipboard.writeText(document.querySelector(`#${id}`).value).then(() => {})
-}
-
-function startAgainScript() {
-	if (confirm('Будет начат новый диалог и сброшены значения динамичных полей')) {
-		dialogStage.startAgain()
-		document.querySelectorAll('.dynamic-field__input.changeable').forEach(e => {
-			e.value = ''
-			document.querySelectorAll(`span.billet-item[data-target=${e.dataset.target}]`).forEach(e => e.innerHTML = '')
-			document.querySelectorAll(`input.billet-item[data-target=${e.dataset.target}`).forEach(e => e.value = '')
+	static start() {
+		// Инициализация контроллера шагов диалога
+		let dialogStageModel = new DialogStageModel(document.querySelector('.dialog__stage.active'))
+		this.dialogStageController = new DialogStageController(dialogStageModel)
+		new StepBackButtonObserver(document.querySelector('#step-back'), dialogStageModel)
+		
+		// Инициализация контроллера динамичных полей
+		this.dynamicFieldsController = new DynamicFieldsController()
+		document.querySelectorAll('.dynamic-field__input').forEach(e => {
+			let group = this.dynamicFieldsController.addGroup(e.dataset.target, '', !e.classList.contains('changeable'))
+			document.querySelectorAll(`[data-target=${group.label}]`).forEach(observer => {
+				if (observer.tagName === 'SPAN') new SpanDynamicFieldObserver(observer, group)
+				else if (observer.tagName === 'INPUT') new InputDynamicFieldObserver(observer, group)
+			})
 		})
 	}
 }
 
-let dialogStage
+
 document.addEventListener('DOMContentLoaded', () => {
-	dialogStage = new DialogStage()
-	document.querySelectorAll(`input[data-target]`).forEach(e => e.addEventListener('keyup', setDynamicValue))
+	Dialog.start()
 })
